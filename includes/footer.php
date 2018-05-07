b27ce0933ec835eb433bb29b847ba1b4469f6848
Iqra Ilyas <iqra.ilyas@kit.ae>
2018-05-07 10:55:35 +0400

    Iqra: Pushing source code to repo

---
+++ includes/footer.php
@@ -0,0 +1,59 @@
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4">
+                <span class="copyright">Copyright &copy; Naresco Travels and Tourism LLC <?php echo date('Y');?></span>
+            </div>
+            <div class="col-md-4">
+                <ul class="list-inline social-buttons">
+                    <li><a href="#"><i class="fa fa-twitter"></i></a>
+                    </li>
+                    <li><a href="#"><i class="fa fa-facebook"></i></a>
+                    </li>
+                    <li><a href="#"><i class="fa fa-linkedin"></i></a>
+                    </li>
+                </ul>
+            </div>
+            <div class="col-md-4">
+                <ul class="list-inline quicklinks">
+                    <li><a href="#">Privacy Policy</a>
+                    </li>
+                    <li><a href="#">Terms of Use</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</footer>
+
+<!-- jQuery -->
+<script src="vendor/jquery/jquery.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+
+<!-- Plugin JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
+<!-- Contact Form JavaScript -->
+<script src="js/jqBootstrapValidation.js"></script>
+<script src="js/contact_me.js"></script>
+
+<!-- Theme JavaScript -->
+<script src="js/agency.min.js"></script>
+
+<script>
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2), zoom: 10
+  };
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+}
+</script>
+
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBu-916DdpKAjTmJNIgngS6HL_kDIKU0aU&callback=myMap"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,264 @@
+<?php
+include_once('includes/header.php');
+?>
+
+<header>
+    <div class="container">
+        <div class="intro-text">
+            <div class="intro-lead-in">Lorem ipsum dolor sit amet</div>
+            <div class="intro-heading">Donec feugiat pulvinar justo, id fermentum.</div>
+            <a href="#services" class="page-scroll btn btn-md"><i class="icon-info icons"></i> Learn more</a>
+        </div>
+    </div>
+</header>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="pad-right">
+					<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia diam et dapibus scelerisque. Nam vel urna ac orci rhoncus pellentesque sit amet eu orci. Morbi tempus placerat lectus, non pretium sem placerat eu. Proin ipsum dui, condimentum non suscipit id, mattis at nibh.</p>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="">
+					<img class="img-responsive" src="img/destinations/london.jpg" style="box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);"/>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="curved-bg-right">
+	<div class="container">
+		<h2>Take a look at our destinations!</h2>
+		<p style="color:#3474FD;font-weight: 700;">View more</p>
+		<hr class="small-line"/>
+		<div class="custom-wrapper gl-1">
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/london.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">London, United Kingdom</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/greece.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Athens, Greece</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/rome.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Rome, Italy</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/india.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Mumbai, India</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/korea.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Jeju Island, South Korea</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/japan.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Tokyo, Japan</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/thailand.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Bangkok, Thailand</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="banner">
+	<h2>Lorem ipsum dolor sit amet</h2>
+</section>
+
+<section class="curved-bg-left">
+	<div class="container">
+		<h2>Latest Special Offers!</h2>
+		<p style="color:#3474FD;font-weight: 700;">View more</p>
+		<hr class="small-line"/>
+		<div class="custom-wrapper gl-2">
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/london.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">London, United Kingdom</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/greece.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Athens, Greece</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/rome.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Rome, Italy</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/india.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Mumbai, India</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/korea.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Jeju Island, South Korea</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+			<div style="background: linear-gradient(to top, rgba(255,0,0,0), rgba(0,0,0,0.75)), url('img/destinations/japan.jpg'); background-size: cover;" class="item">
+				<div class="item-header">
+					<p class="title">Tokyo, Japan</p>
+					<p class="duration">2 days, 1 Night</p>
+					<hr class="small-line line-nomargin"/>
+					<p class="price">Starting from AED 1250</p>
+				</div>
+
+				<div class="item-footer">
+					
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-6">
+				<div class="">
+					<img class="img-responsive" src="img/destinations/london.jpg" style="box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);"/>
+				</div>
+			</div>
+			<div class="col-md-6">
+				<div class="pad-left">
+					<h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h2>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam lacinia diam et dapibus scelerisque. Nam vel urna ac orci rhoncus pellentesque sit amet eu orci. Morbi tempus placerat lectus, non pretium sem placerat eu. Proin ipsum dui, condimentum non suscipit id, mattis at nibh.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<section class="curved-bg-left2">
+	<div class="container">
+		<h2>Want to know more?</h2>
+		<p style="color:#3474FD;font-weight: 700;">We're just a phone call away!</p>
+		<hr class="small-line"/>
+		<div class="row">
+			<div class="col-md-6">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3608.6457679843143!2d55.30130331501055!3d25.248853383872085!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3e5f43262145d7a1%3A0x6d3d0bebe8101d33!2sNaresco+Travel+%26+Tourism+LLC!5e0!3m2!1sen!2sae!4v1524546928360" width="100%" height="500px" frameborder="0" style="border:0;border-radius:4px;box-shadow: 0 10px 20px rgba(0,0,0,0.19), 0 6px 6px rgba(0,0,0,0.23);" allowfullscreen></iframe>
+			</div>
+
+			<div class="col-md-6">
+				<div class="pad-left">
+					<div>
+						<p style="font-size:17px;font-weight: 700;"><i style="font-size:17px;font-weight:700;color:#3474FD;" class="icon-phone icons"></i>+971 4 123 4567</p>
+						<p style="font-size:17px;font-weight: 700;"><i style="font-size:17px;font-weight:700;color:#3474FD;" class="icon-envelope icons"></i>info@ntt.ae</p>
+						<p style="font-size:17px;font-weight: 700;"><i style="font-size:17px;font-weight:700;color:#3474FD;" class="icon-map icons"></i>Street #4B, Al Karama, Dubai, U.A.E</p>
+					</div>
+					<hr class="small-line"/>
+					<div>
+						<p style="font-weight: 700;">If you have any queries, please provide us your details and we will get back to you as soon as possible!</p>
+						<br/>
+						<input type="text" placeholder="First Name" class="form-control"/><br/>
+						<input type="text" placeholder="Last Name" class="form-control"/><br/>
+						<input type="text" placeholder="Phone Number" class="form-control"/><br/>
+						<button class="btn btn-md"><i class="icon-phone icons"></i> Contact us!</a>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<?php
+include_once('includes/footer.php');
+?>
